86ec2b3a4ab83de05ec6b820c472a5331a88ee64
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-04-28 23:36:55 +0300

    Finish Debug-Codes/Distance

--- Arduino/Distance/Distance.ino
+++ Arduino/Distance/Distance.ino
@@ -5,40 +5,39 @@
 
 #define motorPin 3
 
+#define DEBUG // Reduced rotation angle and mimics stand by
+#define NO_PHONE // For testing without reading any serial input
+ 
 #define soundConstant 0.034
 
 Servo motor;
-int distanceCm ;
+int cmDistance;
+int rotateAngle = 0;
+char standByVal = '1'; // MIT App Inventor will send '0' for pause radar
 
 void setup()
 {
-
   #define ROTATE_ANGLE 180
 
-
   motor.attach(motorPin);
- 
+  
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT);
+     
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if(Serial.available() > 0)
-    standByVal = Serial.read();
-
-  for (int i = 0; i <= ROTATE_ANGLE; i++)
+  for (rotateAngle = 0; rotateAngle <= ROTATE_ANGLE; rotateAngle++)
     mainEvent();
 
-  for (int i = ROTATE_ANGLE; i >= 0; i--)
+  for (rotateAngle = ROTATE_ANGLE; rotateAngle >= 0; rotateAngle--)
     mainEvent();
 }
 
-
 int distCalc()
 {
-
   long timeVal;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -48,13 +47,35 @@ int distCalc()
 
   timeVal = pulseIn(echoPin, HIGH);
   
-  distanceCm = timeVal * soundConstant / 2;
-  return distanceCm;
+  cmDistance = timeVal * soundConstant / 2;
+  
+  return cmDistance;
+}
+
+
+inline boolean objectInRange()
+{
+  return cmDistance < 320; // TODO Find Real Life Value for 320
 }
 
 void mainEvent()
 {
   motor.write(i);
   distCalc();
-  Serial.println(distanceCm);
+  ledRedOrGreen();
+  builtinLed();
+  #ifndef DEBUG
+    buzzOrNot(cmDistance);
+  #endif
+  #ifndef NO_PHONE
+    standBy();
+  #endif
+  // if (objectInRange()) // TODO Only send when object detected
+  send_packet();
+}
+void send_packet()// TODO Maybe encapsulate with a struct
+{
+  Serial.print(rotateAngle);
+  Serial.print(',');
+  Serial.println(cmDistance);// println adds \n for indicating one packet has send
 }
